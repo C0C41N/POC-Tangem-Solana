98c7cd9bfe79d82efd157604c1197fa8da299ea6
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-09-11 12:54:52 +0500

    updates

---
+++ POC Tangem Solana/Actions.swift
@@ -0,0 +1,14 @@
+//
+//  Actions.swift
+//  POC Tangem Solana
+//
+//  Created by Ali M. on 11/09/2024.
+//
+
+import Foundation
+
+class Actions {
+    func scan() {
+        print("Hello World!")
+    }
+}
